9e34067da87b04375fcdedac2028f47a08bd7810
MicaDorado <micadorado2016@gmail.com>
2025-11-12 09:11:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5174"],
+    origin: ["http://localhost:3000", "http://localhost:5175"],
     credentials: true,
   }),
 );
